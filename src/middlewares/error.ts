0da8bd09aef922baef979f0240cb2cd84cdbb746
bjayzee <bjayzee@gmail.com>
2025-01-06 10:59:39 +0100

    cors origin fix

--- src/middlewares/error.ts
+++ src/middlewares/error.ts
@@ -1,9 +1,9 @@
-import { Request, Response, NextFunction } from 'express';
+import { Request, Response } from 'express';
 import { sendResponse } from '../utils/response';
 import httpStatus from 'http-status';
 
 
-export const errorHandler = (err: Error, req: Request, res: Response, next: NextFunction) => {
+export const errorHandler = (err: Error, req: Request, res: Response) => {
     req.log.error({ err }, "Unhandled error occurred");
     sendResponse(res, httpStatus.INTERNAL_SERVER_ERROR, false, err.message);
 };
