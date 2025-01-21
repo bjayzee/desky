6619b277858c825f4a1ef48a673bffb3a193cffe
AV2001 <anirudhvadlamani2001@gmail.com>
2025-01-21 18:16:01 +0400

    set expiresIn to be the value of JWT_EXPIRE_TIME from .env

--- src/utils/jwt.ts
+++ src/utils/jwt.ts
@@ -1,20 +1,18 @@
 import jwt from 'jsonwebtoken';
 
-
-
 const JWT_SECRET = process.env.JWT_SECRET as string;
 const JWT_EXPIRE_TIME = process.env.JWT_EXPIRE_TIME as string;
 
 interface JwtPayload {
-    agencyId: string;
-    email: string;
-    role: string;
+  agencyId: string;
+  email: string;
+  role: string;
 }
 
-export const generateJwt = (payload: JwtPayload, expiresIn: string = JWT_EXPIRE_TIME) => {
-    return jwt.sign(payload, JWT_SECRET, { expiresIn });
+export const generateJwt = (payload: JwtPayload) => {
+  return jwt.sign(payload, JWT_SECRET, { expiresIn: JWT_EXPIRE_TIME });
 };
 
-export const decodeJwt = (token: string) =>{
-    return jwt.verify(token, JWT_SECRET);
-}
+export const decodeJwt = (token: string) => {
+  return jwt.verify(token, JWT_SECRET);
+};
